fce37819095cadd7a546b070baf8aa0da5b8a2de
annndrey <annndrey@trololo.info>
2019-08-25 22:25:07 +0300

    added client-side scripts

---
+++ api/config_dev.py
@@ -0,0 +1,6 @@
+DEBUG = True
+DEVELOPMENT = True
+SECRET_KEY='secretkeyformyapp'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://plantdata:plantdatapass@192.168.1.4/plantdb'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+FILE_PATH = "/media/MEDIA/PLANTDATA"
