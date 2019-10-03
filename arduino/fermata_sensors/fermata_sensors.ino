58453672bead8fe3ed24dbd0ff6f9cb2380c6751
Dan Kazaryan <bugoon@gmail.com>
2019-10-03 06:16:55 -0700

    Changed amount of weight sensors (from one to five) Changed sensor pinout a bit (DHT0 and DHT1 at pins 12 and 13 correspondingly). Weight sensors on pins from 2 to 11

--- arduino/fermata_sensors/fermata_sensors.ino
+++ arduino/fermata_sensors/fermata_sensors.ino
@@ -2,11 +2,12 @@
 #include <DHT.h>
 #include <HX711.h>
 #include <Digital_Light_TSL2561.h>
-#define DHTPIN0 2
+#define DHTPIN0 12
 #define DHTTYPE0 DHT22
-#define DHTPIN1 4
+#define DHTPIN1 13
 #define DHTTYPE1 DHT22
 #define PIN_MQ135  A5
+#define WEIGHT_SENSORS_AMOUNT 5
 
 int sensorValue;
 int wght;
@@ -18,12 +19,12 @@ float bar_pressure;
 float bar_temp;
 
 //load cell section
-const int LOADCELL_DOUT_PIN = 6;
-const int LOADCELL_SCK_PIN = 5;
+const int LOADCELL_DOUT_PIN[WEIGHT_SENSORS_AMOUNT] = {3, 5, 7, 9, 11};
+const int LOADCELL_SCK_PIN[WEIGHT_SENSORS_AMOUNT] = {2, 4, 6, 8, 10};
 float calibration_factor = 452;
 long offset = 0;
 float scaleunits;
-double scalevalue;
+double scalevalue[WEIGHT_SENSORS_AMOUNT];
 
 //load cell section
 
@@ -40,7 +41,7 @@ const int R0 = 100000; //B value of the thermistor
 
 DHT dht0(DHTPIN0, DHTTYPE0);
 DHT dht1(DHTPIN1, DHTTYPE0);
-HX711 scale;
+HX711 scale[WEIGHT_SENSORS_AMOUNT];
 int scale_factor = 911100;
 
 void setup()
@@ -55,15 +56,19 @@ void setup()
   //Serial.println("DHT0 Initialization completed");
   dht1.begin();
   TSL2561.init();
-  //Serial.println("DHT1 Initialization completed");
+  Serial.println("DHT1 Initialization completed");
   //Serial.println("Barometer Initialization completed");
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN, 64);
-  scale.set_scale();
-  scale.set_offset();
+  //Initialazing weight sensors
+  for(int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
+  {
+    scale[i].begin(LOADCELL_DOUT_PIN[i], LOADCELL_SCK_PIN[i], 64);
+    scale[i].set_scale();
+    scale[i].set_offset();
+  }
   //Code added by Dan 18/9/2019
   //offset = scale.get_offset();
   //offset = offset - (50*calibration_factor); //substract 50 gramm weight from offset
-  scale.set_offset(offset);
+  //scale.set_offset(offset);
   //scale.begin(A1, A0);
   //scale.set_scale(-15920);
   //scale.set_scale();
@@ -77,12 +82,11 @@ void loop()
 
   sensorValue = analogRead(0);
   //wght = analogRead(2);
-
   float h0 = dht0.readHumidity();   // считывание влажности
   float t0 = dht0.readTemperature();  // считывание температуры
   float h1 = dht1.readHumidity();   // считывание влажности
   float t1 = dht1.readTemperature();  // считывание температуры
-
+  Serial.println("Brake5");
   bar_pressure = 0;//barometer.readPressureMillibars();
   bar_temp = 0;//barometer.readTemperatureC();
 
@@ -104,26 +108,24 @@ void loop()
   long uvmean = uvsum / 1024;
   uv = (uvmean * 1000 / 4.3 - 83) / 21;
   uv = -1;
-  
-  //scale
-  scaleunits = scale.get_units(10);
-  scalevalue = scale.get_value(10);
-  if (scaleunits < 0)
-    {
-      scaleunits = 0.00;
-    }
 
-  //scale
+  //
+  for(int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
+  {
+    scalevalue[i] = scale[i].get_value(10);
+  }
   
-  //float current_weight=scale.get_units(20);
-  //float scale_factor=(current_weight/50);
-  //Serial.print("Scale: " );
-  //Serial.println(scale_factor);
-  //Serial.println(current_weight);
+  //Data sending section
+  
   Serial.print("{");
-  Serial.print("'WGHT0' : ");
-  Serial.print(scalevalue);
-  Serial.print(", ");
+  for(int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
+  {
+    Serial.print("'WGHT");
+    Serial.print(i);
+    Serial.print("' : ");
+    Serial.print(scalevalue[i]);
+    Serial.print(", ");
+  }
   Serial.print("'T0' : ");
   Serial.print(t0);
   Serial.print(", ");
