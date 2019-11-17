68198507a8c08b37ea6c91f1cf6dc6b36fab81b8
annndrey <annndrey@trololo.info>
2019-11-17 03:21:55 +0300

    updated client's models

--- client/models.py
+++ client/models.py
@@ -6,11 +6,20 @@ import datetime
 
 Base = declarative_base()
 
+#class Weight(Base):
+#    __tablename__ = 'weight'
+#    id = Column(Integer, primary_key=True)
+#    label = Column(String(255))
+#    value = Column(Integer)
+#    ts = Column(DateTime, default=datetime.datetime.now)
+
+    
 class SensorData(Base):
     __tablename__ = 'sensordata'
     id = Column(Integer, primary_key=True)
     ts = Column(DateTime, default=datetime.datetime.now)
     sensor_uuid = Column(Text(), nullable=False)
+    remote_data_id = Column(Integer)
     # some data here
     temp0 = Column(Numeric(precision=3))
     temp1 = Column(Numeric(precision=3))
@@ -22,13 +31,20 @@ class SensorData(Base):
     soilmoist = Column(Integer)
     co2 = Column(Integer)
     wght0 = Column(Numeric(precision=3))
-    photos = relationship("Photo", backref="sensordata")
+    wght1 = Column(Numeric(precision=3))
+    wght2 = Column(Numeric(precision=3))
+    wght3 = Column(Numeric(precision=3))
+    wght4 = Column(Numeric(precision=3))
+    photos = relationship("Photo", backref="sensordata", cascade="all,delete")
     uploaded = Column(Boolean, default=False)
-
+    
+    
 class Photo(Base):
     __tablename__ = 'photo'
     photo_id = Column(Integer, primary_key=True)
     sensordata_id = Column(Integer, ForeignKey('sensordata.id'))
     photo_filename  = Column(Text())
     label = Column(String(255))
-    
+    uploaded = Column(Boolean, default=False)
+    camname = Column(String(255))
+    camposition = Column(Integer)
