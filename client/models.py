a3c185cae43d9c4f450048eabf504d8b60ad0ac3
annndrey <annndrey@trololo.info>
2020-01-30 21:17:12 +0300

    Issue #46 Modified data structrue on RPi, now there's basestation->probe->probedata relations

--- client/models.py
+++ client/models.py
@@ -8,53 +8,37 @@ Base = declarative_base()
 
 # TODO Add Probe & ProbeData
 
-#class Probe(Base):
-#    id = Column(Integer, primary_key=True)
-#    uuid = Column(Text(), nullable=False)
-#    data_id = Column(Integer, ForeignKey('sensor_data.id'))
-#    data = relationship("SensorData", backref=backref("probes", uselist=True))
-#    ptype = Column(String(200))
-#    label = Column(String(200))
-#    minvalue = Column(Numeric(precision=3))
-#    maxvalue = Column(Numeric(precision=3))
+class Probe(Base):
+    __tablename__ = 'probe'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(Text(), nullable=False)
+    bs_id = Column(Integer, ForeignKey('bsdata.id'))
+    basestation = relationship("BaseStationData", backref=backref("probes", uselist=True))
 
-
-#class ProbeData(Base):
-#    d = Column(Integer, primary_key=True)
-#    probe_id = Column(Integer, ForeignKey('probe.id'))
-#    probe = relationship("Probe", backref=backref("values", uselist=True))
-#    value = Column(Numeric(precision=3))
+class ProbeData(Base):
+    __tablename__ = 'probedata'
+    id = Column(Integer, primary_key=True)
+    probe_id = Column(Integer, ForeignKey('probe.id'))
+    probe = relationship("Probe", backref=backref("values", uselist=True))
+    value = Column(Numeric(precision=3))
+    ptype = Column(String(200))
+    label = Column(String(200))
     
 
-class SensorData(Base):
-    __tablename__ = 'sensordata'
+class BaseStationData(Base):
+    __tablename__ = 'bsdata'
     id = Column(Integer, primary_key=True)
     ts = Column(DateTime, default=datetime.datetime.now)
-    sensor_uuid = Column(Text(), nullable=False)
+    bs_uuid = Column(Text(), nullable=False)
     remote_data_id = Column(Integer)
-    # some data here
-    temp0 = Column(Numeric(precision=3))
-    temp1 = Column(Numeric(precision=3))
-    hum0 = Column(Numeric(precision=3))
-    hum1 = Column(Numeric(precision=3))
-    tempA = Column(Numeric(precision=3))
-    uv = Column(Integer)
-    lux = Column(Integer)
-    soilmoist = Column(Integer)
-    co2 = Column(Integer)
-    wght0 = Column(Numeric(precision=3))
-    wght1 = Column(Numeric(precision=3))
-    wght2 = Column(Numeric(precision=3))
-    wght3 = Column(Numeric(precision=3))
-    wght4 = Column(Numeric(precision=3))
-    photos = relationship("Photo", backref="sensordata", cascade="all,delete")
+    photos = relationship("Photo", backref="bs", cascade="all,delete")
     uploaded = Column(Boolean, default=False)
     
     
 class Photo(Base):
     __tablename__ = 'photo'
     photo_id = Column(Integer, primary_key=True)
-    sensordata_id = Column(Integer, ForeignKey('sensordata.id'))
+    bs_id = Column(Integer, ForeignKey('bsdata.id'))
     photo_filename  = Column(Text())
     label = Column(String(255))
     uploaded = Column(Boolean, default=False)
--- client/test_esp_api.py
+++ client/test_esp_api.py
@@ -4,21 +4,30 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
 def get_random(a=1, b=100, roundto=2):
     return round(random.uniform(a, b), roundto)
 
+def get_macaddr():
+    mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255),
+                                 random.randint(0, 255),
+                                 random.randint(0, 255))
+    return mac
+
 @app.get("/sensor_data")
 async def info():
-    data = [{"UUID":"24:6F:28:97:0F:54"},
-            {"ptype":"temp","label":"T0","value": get_random(16.4, 23.5)},
-            {"ptype":"temp","label":"T1","value": get_random(18.4, 22.5)},
-            {"ptype":"temp","label":"T2","value": get_random(17.4, 18.5)},
-            {"ptype":"humid","label":"H0","value": get_random(60.3, 70.3)},
-            {"ptype":"humid","label":"H1","value": get_random(65.3, 69.3)},
-            {"ptype":"pres","label":"P0","value": get_random(100, 101, 0)},
-            {"ptype":"co2","label":"C0","value": get_random(200, 300, 0)},
-            {"ptype":"co2","label":"C1","value": get_random(200, 300, 0)},
-            {"ptype":"light","label":"L0","value": get_random(0, 100)}
-    ]
+    data = { "UUID": get_macaddr(),
+             "data": [
+                 {"ptype":"temp","label":"T0","value": get_random(16.4, 23.5)},
+                 {"ptype":"temp","label":"T1","value": get_random(18.4, 22.5)},
+                 {"ptype":"temp","label":"T2","value": get_random(17.4, 18.5)},
+                 {"ptype":"humid","label":"H0","value": get_random(60.3, 70.3)},
+                 {"ptype":"humid","label":"H1","value": get_random(65.3, 69.3)},
+                 {"ptype":"pres","label":"P0","value": get_random(100, 101, 0)},
+                 {"ptype":"co2","label":"C0","value": get_random(200, 300, 0)},
+                 {"ptype":"co2","label":"C1","value": get_random(200, 300, 0)},
+                 {"ptype":"light","label":"L0","value": get_random(0, 100)}
+             ]
+    }
 
     return data
