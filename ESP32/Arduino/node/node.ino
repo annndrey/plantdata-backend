6f04abe99ed19a3ab073ddf148bcc61a02e87a64
Dan <bugoon@gmail.com>
2020-01-31 05:47:30 -0800

    Changes in ESP32 code: -JSON respond structure changed corresponding to issue #55 -Added ds18b20 temp sensors support -Added BMP280 temp and pressure sensore support

--- ESP32/Arduino/node/node.ino
+++ ESP32/Arduino/node/node.ino
@@ -1,9 +1,12 @@
-#define TEST 1
+#define TEST true   //Set TRUE for testing
 
 #include <ArduinoJson.h>
 
 #include <EEPROM.h>
 
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <Adafruit_BMP280.h>
 #include <Digital_Light_ISL29035.h>
 #include <Digital_Light_TSL2561.h>
 #include <DHT.h>
@@ -18,12 +21,13 @@
 #include <HX711.h>
 #include "pins_ESP.h"
 
+#define ONE_WIRE_BUS D3 //pin for Dallas temperature sensors
 #define DHTPIN0 D21
 #define DHTTYPE0 DHT22
 #define DHTPIN1 3
 #define DHTTYPE1 DHT22
-#define PIN_MQ135  ADC2
-#define WEIGHT_SENSORS_AMOUNT 2
+#define CO2_0  ADC2
+#define WEIGHT_SENSORS_AMOUNT 0
 
 IPAddress local_ip(192, 168, 1, 140);
 IPAddress gateway(192, 168, 0, 254);
@@ -48,10 +52,14 @@ const int LOADCELL_SCK_PIN[6] = {D3, D4, D5, D6, D7, D8};
 const int B = 4275;
 const int R0 = 100000; //B value of the thermistor
 
+Adafruit_BMP280 bmp; 
 DHT dht0(DHTPIN0, DHTTYPE0);
 DHT dht1(DHTPIN1, DHTTYPE0);
 HX711 scale[WEIGHT_SENSORS_AMOUNT];
 int scale_factor = 911100;
+OneWire oneWire(ONE_WIRE_BUS);// Setup a oneWire instance to communicate with a OneWire device
+DallasTemperature tempSensors(&oneWire);// Pass our oneWire reference to Dallas Temperature sensor 
+DeviceAddress sensor0 = {0x28, 0x62, 0xDA, 0x05, 0x00, 0x00, 0x00, 0xD1};
 
 void setup()
 {
@@ -72,7 +80,7 @@ void setup()
   Serial.println(WiFi.localIP());
   //OTA check section
   MDNS.begin(host);
-  //OTA update handler
+  //OTA update handler section
   server.on("/update", HTTP_POST, []() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
@@ -100,15 +108,32 @@ void setup()
       Serial.printf("Update Failed Unexpectedly (likely broken connection): status=%d\n", upload.status);
     }
   });
-  //
+  
+  //Sensors init section
+  tempSensors.begin();
+  
   pinMode(DHTPIN0, INPUT);
   Wire.begin();
   dht0.begin();  //  запуск датчика DHT
-  Serial.println("DHT0 ready");
+  
   dht1.begin();
   TSL2561.init();
   Serial.println("Light ready!");
 
+  //BMP280 init section
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+  // Default settings from datasheet. 
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode
+                  Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling 
+                  Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling 
+                  Adafruit_BMP280::FILTER_X16,      // Filtering
+                  Adafruit_BMP280::STANDBY_MS_500); // Standby time
+
+  //Scales init section
   for (int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
   {
     scale[i].begin(LOADCELL_DOUT_PIN[i], LOADCELL_SCK_PIN[i], 64);
@@ -117,6 +142,7 @@ void setup()
     Serial.println("WGHT ready");
   }
 
+  //webserver init section
   server.on("/sensor_data", handle_SendSensorData);
   server.on("/info", handle_SendInfo);
   server.begin();
@@ -157,70 +183,89 @@ void handle_SendInfo() {
 
 String ReadSensors()  {
   Serial.println("Reading Sensors!");
-  int sensorCO2Value = analogRead(PIN_MQ135);
-  sensorCO2Value = map(sensorCO2Value, 0, 4095, 0, 1023); //because ESP32 has 12-bit ADC
-  float h0 = dht0.readHumidity();   // считывание влажности
-  float t0 = dht0.readTemperature();  // считывание температуры
-  float h1 = -1;   // считывание влажности
-  float t1 = -1;  // считывание температуры
-  unsigned long lght = TSL2561.readVisibleLux();
-  //Reading temperature sensor
+  int c0 = analogRead(CO2_0); //reading CO2 sensor values
+  c0 = map(c0, 0, 4095, 0, 1023); //because ESP32 has 12-bit ADC
+  
+  float h0 = dht0.readHumidity();   // reading humidity
+  float t0 = dht0.readTemperature();  // reading temperature from humidity sensor
+  
+  float p0 = bmp.readPressure();   // reading pressure from BMP280
+  float t1 = bmp.readTemperature();  // reading temperature from BMP280
+
+  tempSensors.requestTemperatures(); // Send the command to get temperatures from Dallas temp sensors
+  float t2 = tempSensors.getTempC(sensor0);
+  
+  unsigned long l0 = TSL2561.readVisibleLux();
+  
+  /*Reading temperature sensor
   int tmpA = analogRead(ADC1);
   float Rt0 = 4095.0 / tmpA - 1.0; //using 4095, because ESP32 has 12-bit ADC
   Rt0 = R0 * Rt0;
   float tempA = 1.0 / (log(Rt0 / R0) / B + 1 / 298.15) - 273.15;
-  //
+  */
+  
   double scalevalue[WEIGHT_SENSORS_AMOUNT];
   for (int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
   {
     scalevalue[i] = scale[i].get_value(10);
   }
 
-  const int json_capacity = JSON_OBJECT_SIZE(8 + WEIGHT_SENSORS_AMOUNT);
-  StaticJsonDocument<json_capacity> doc;
-  doc["T0"] = t0;
-  doc["H0"] = h0;
-  doc["T1"] = t1;
-  doc["H1"] = h1;
-  doc["TA"] = tempA;
-  doc["L"] = lght;
-  doc["CO2"] = sensorCO2Value;
-  for (int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
-  {
-    String key = String("WGHT" + String(i));
-    doc[key] = scalevalue[i];
-  }
-  String sensorData;
-  serializeJson(doc, sensorData);
-  return sensorData;
+  const int json_capacity = JSON_ARRAY_SIZE(10) + 10*JSON_OBJECT_SIZE(3);
+  StaticJsonDocument<json_capacity> info;
+  info["UUID"] = String(WiFi.macAddress());
+  JsonArray pdata = info.createNestedArray("data");
+  
+  JsonObject probe0 = pdata.createNestedObject();
+  probe0["ptype"] = "temp";
+  probe0["label"] = "T0";
+  probe0["value"] = t0;
+
+  JsonObject probe1 = pdata.createNestedObject();
+  probe1["ptype"] = "temp";
+  probe1["label"] = "T1";
+  probe1["value"] = t1;
+
+  JsonObject probe2 = pdata.createNestedObject();
+  probe2["ptype"] = "temp";
+  probe2["label"] = "T2";
+  probe2["value"] = t2;
+
+  JsonObject probe3 = pdata.createNestedObject();
+  probe3["ptype"] = "humid";
+  probe3["label"] = "H0";
+  probe3["value"] = h0;
   /*
-    String sensorData;
-    sensorData.concat("{'T0' : ");
-    sensorData.concat(t0);
-    sensorData.concat(", 'H0': ");
-    sensorData.concat(h0);
-    sensorData.concat(", 'T1': ");
-    sensorData.concat(t1);
-    sensorData.concat(", 'H1': ");
-    sensorData.concat(h1);
-    sensorData.concat(", 'TA': ");
-    sensorData.concat(tempA);
-    sensorData.concat(", 'L': ");
-    sensorData.concat(lght);
-    sensorData.concat(", 'CO2': ");
-    sensorData.concat(sensorCO2Value);
-    for (int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
-    {
-    sensorData.concat(", ");
-    sensorData.concat("'WGHT");
-    sensorData.concat(i);
-    sensorData.concat("': ");
-    sensorData.concat(scalevalue[i]);
-    }
-    sensorData.concat("}");
-    return sensorData;
+  JsonObject probe4 = pdata.createNestedObject();
+  probe4["ptype"] = "humid";
+  probe4["label"] = "H1";
+  probe4["value"] = -1;
   */
+  JsonObject probe5 = pdata.createNestedObject();
+  probe5["ptype"] = "pres";
+  probe5["label"] = "P0";
+  probe5["value"] = p0;
+
+  JsonObject probe6 = pdata.createNestedObject();
+  probe6["ptype"] = "co2";
+  probe6["label"] = "C0";
+  probe6["value"] = c0;
+  /*
+  JsonObject probe7 = pdata.createNestedObject();
+  probe7["ptype"] = "co2";
+  probe7["label"] = "C1";
+  probe7["value"] = -1;
+  */
+  JsonObject probe8 = pdata.createNestedObject();
+  probe8["ptype"] = "light";
+  probe8["label"] = "L0";
+  probe8["value"] = l0;
+  
+  String sensorData;
+  serializeJson(info, sensorData);
+  return sensorData;
 }
+
+
 
 String SendInfo() {
   const int json_capacity = JSON_OBJECT_SIZE(9);
@@ -235,22 +280,60 @@ String SendInfo() {
   return infoData;
 }
 
+
+
 String Test() {
-  const int json_capacity = JSON_OBJECT_SIZE(8 + WEIGHT_SENSORS_AMOUNT);
-  StaticJsonDocument<json_capacity> doc;
-  doc["T0"] = -1;
-  doc["H0"] = -1;
-  doc["T1"] = -1;
-  doc["H1"] = -1;
-  doc["TA"] = -1;
-  doc["L"] = -1;
-  doc["CO2"] = -1;
-  for (int i = 0; i < WEIGHT_SENSORS_AMOUNT; i++)
-  {
-    String key = String("WGHT" + String(i));
-    doc[key] = -1;
-  }
+  const int json_capacity = JSON_ARRAY_SIZE(10) + 10*JSON_OBJECT_SIZE(3);
+  StaticJsonDocument<json_capacity> info;
+  info["UUID"] = String(WiFi.macAddress());
+  JsonArray pdata = info.createNestedArray("data");
+  
+  JsonObject probe0 = pdata.createNestedObject();
+  probe0["ptype"] = "temp";
+  probe0["label"] = "T0";
+  probe0["value"] = -1;
+
+  JsonObject probe1 = pdata.createNestedObject();
+  probe1["ptype"] = "temp";
+  probe1["label"] = "T1";
+  probe1["value"] = -1;
+
+  JsonObject probe2 = pdata.createNestedObject();
+  probe2["ptype"] = "temp";
+  probe2["label"] = "T2";
+  probe2["value"] = -1;
+
+  JsonObject probe3 = pdata.createNestedObject();
+  probe3["ptype"] = "humid";
+  probe3["label"] = "H0";
+  probe3["value"] = -1;
+
+  JsonObject probe4 = pdata.createNestedObject();
+  probe4["ptype"] = "humid";
+  probe4["label"] = "H1";
+  probe4["value"] = -1;
+
+  JsonObject probe5 = pdata.createNestedObject();
+  probe5["ptype"] = "pres";
+  probe5["label"] = "P0";
+  probe5["value"] = -1;
+
+  JsonObject probe6 = pdata.createNestedObject();
+  probe6["ptype"] = "co2";
+  probe6["label"] = "C0";
+  probe6["value"] = -1;
+
+  JsonObject probe7 = pdata.createNestedObject();
+  probe7["ptype"] = "co2";
+  probe7["label"] = "C1";
+  probe7["value"] = -1;
+
+  JsonObject probe8 = pdata.createNestedObject();
+  probe8["ptype"] = "light";
+  probe8["label"] = "L0";
+  probe8["value"] = -1;
+  
   String sensorData;
-  serializeJson(doc, sensorData);
+  serializeJson(info, sensorData);
   return sensorData;
 }
