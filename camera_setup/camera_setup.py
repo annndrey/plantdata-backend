eb4f6ef871ea99d7bef07d47488181382259be25
annndrey <annndrey@trololo.info>
2020-02-20 23:42:37 +0300

    Updated camera_setup app, now it usees opencv to process the camera's mjpeg stream

--- camera_setup/camera_setup.py
+++ camera_setup/camera_setup.py
@@ -7,26 +7,54 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 import time
+from threading import Thread
+import cv2
 
+base_url = "http://admin:plantdata@cf-fungi-cam1.ngrok.io"
 
-base_url = "http://admin:plantdata@cf-fungi-cam6.ngrok.io"
+def on_change(e):
+    print(e)
+    global base_url
+    base_url = e.widget.get()
+    #open_img(url)
 
+        
 def open_img():
-    #img_url = "https://placeimg.com/640/480/any"
+    global base_url
     img_url = base_url + "/jpgimage/1/image.jpg"
-    response = requests.get(img_url, stream=True, timeout=3)
-    #img_data = response.content
-    response.decode_content = True
+    cap = cv2.VideoCapture(img_url)
+    _, frame = cap.read()
+    #frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
     
-    print(response.status_code)
-    with BytesIO() as fdata:
-        for block in response.iter_content(1024):
-            fdata.write(block)
-        img = ImageTk.PhotoImage(Image.open(fdata))
-    label1.image = img
-    label1.configure(image = img)
-    root.update_idletasks()
-    root.after(500, open_img)
+    label1.image = imgtk
+    label1.configure(image = imgtk)
+    label1.after(50, open_img)
+    #img_url = base_url + "/jpgimage/1/image.jpg"
+    #response = requests.get(img_url)
+    #print(response.status_code)
+    #img_data = response.content
+    #img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+    #root.update_idletasks()
+    #
+    #root.after(50, open_img)
+    #img_url = base_url + "/jpgimage/1/image.jpg"
+    #response = requests.get(img_url, stream=True, timeout=3)
+    #img_data = response.content
+    #response.decode_content = True
+    #print(response.status_code)
+    #with BytesIO() as fdata:
+    #    for block in response.iter_content(1024):
+    #        fdata.write(block)
+    #    img_obj = Image.open(fdata)
+    #    #img_obj = img_obj.resize((640, 480), Image.ANTIALIAS)
+    #    img = ImageTk.PhotoImage(img_obj)
+    #    label1.image = img
+    #    label1.configure(image = img)
+    #root.update_idletasks()
+    #root.after(100, open_img())
 
 
 def ir_command(cmd):
@@ -52,7 +80,7 @@ def move_command(cmd):
     }
     requests.post(cmd_url, data=cmd_data)
     cmd_data['command'] = 0
-    time.sleep(0.5)
+    time.sleep(0.3)
     requests.post(cmd_url, data=cmd_data)
 
 def preset_command(preset_num, cmd):
@@ -72,7 +100,6 @@ def preset_command(preset_num, cmd):
   
     
 root = tkinter.Tk()
-    
 label1 = tkinter.Label(root, text = "")
 label1.grid(row=0, column=0, columnspan=6, sticky=W+E+N+S, padx=5, pady=5)#pack()
 
@@ -107,6 +134,15 @@ b_l_on = tkinter.Button(root, text = "IR on", command = lambda: ir_command(1))
 b_l_off = tkinter.Button(root, text = "IR off", command = lambda: ir_command(0))
 b_l_on.grid(row=2, column=4, sticky=W+E)
 b_l_off.grid(row=2, column=5, sticky=W+E)
+
+
+host_label = Label(root, text="camera URL")
+host_label.grid(row=3, column=0, sticky=W+E)
+entryText = tkinter.StringVar()
+entry = tkinter.Entry(root, textvariable=entryText)
+entryText.set(base_url)
+entry.grid(row=3, column=1, columnspan = 5, sticky=W+E)
+entry.bind("<Return>", on_change)
 
 open_img()
 
