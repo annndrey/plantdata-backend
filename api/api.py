78e40027ad7150c1122f98db5d3d0c491cb27d69
annndrey <annndrey@trololo.info>
2020-03-04 00:06:13 +0300

    ci test 6

--- api/api.py
+++ api/api.py
@@ -74,6 +74,7 @@ db.init_app(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
 
+
 gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
