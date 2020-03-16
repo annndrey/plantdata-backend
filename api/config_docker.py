495dbb977fea3eab96b2f3a9ca49a0ef1f9c070d
annndrey <annndrey@trololo.info>
2020-03-16 15:45:23 +0300

    updated db connection string

--- api/config_docker.py
+++ api/config_docker.py
@@ -1,7 +1,7 @@
 DEBUG = True
 DEVELOPMENT = True
 SECRET_KEY='secretkeyformyapp'
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://plantdata:plantdatapass@192.168.1.4/plantdb_copy'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://plantdata:plantdatapass@192.168.1.4/plantdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 FILE_PATH = "/data/picts"
 CF_LOGIN = "saladuser"
