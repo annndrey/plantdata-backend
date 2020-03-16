f183765133c4393ec49dcf43b8e5892dbc302829
annndrey <annndrey@trololo.info>
2020-03-17 02:51:42 +0300

    test

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,12 +3,12 @@
 # Wait for database
 # chmod +x wait-for-it && ./wait-for-it database:3306
 # Prepare and start webserver
-rm -r migrations
+#rm -r migrations
 export PYTHONDONTWRITEBYTECODE=1
-flask db init
-flask db migrate
+#flask db init
+#flask db migrate
 #export PYTHONPYCACHEPREFIX="/tmp/.cache/cpython/"
-flask db upgrade
+#flask db upgrade
 #ls /data
 date
 uwsgi --ini uwsgi-docker.ini --socket :8000 --protocol=http 
