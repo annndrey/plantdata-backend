5e84c4b0ad899a0373fd8d09780c8668361f5bb5
annndrey <annndrey@trololo.info>
2020-03-17 03:20:30 +0300

    test

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -4,7 +4,7 @@
 # Wait for database
 # chmod +x wait-for-it && ./wait-for-it database:3306
 # Prepare and start webserver
-rm -r migrations
+sudo rm -r migrations
 export PYTHONDONTWRITEBYTECODE=1
 mysql -u $DBUSER -h $HOST_ADDR -p$DBPASS -e "DROP TABLE alembic_version" $DBNAME
 flask db init
