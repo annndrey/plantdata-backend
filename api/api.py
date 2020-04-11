cb755871df3ee030928fc11ba4011cd3bce3ed46
Andrew <gontchar@gmail.com>
2020-04-11 16:36:39 +0300

    test

--- api/api.py
+++ api/api.py
@@ -2167,7 +2167,7 @@ class DataAPI(Resource):
         # If there's no registered CAMNAME & CAMPOSITION for a given data.uuid
         # add new camera & position
         # >>>
-        app.logger.debug(["Request Data", request.values, request.files])
+        app.logger.debug(["Request Data", request.values, request.files, request.json])
         camname = request.form.get("camname")
         camposition = request.form.get("camposition")
         recognize = request.form.get("recognize", False)
