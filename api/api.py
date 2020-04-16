6709d00f147393ce0f83ff0c5f4949d778ae4c6d
Andrew <gontchar@gmail.com>
2020-04-17 00:50:18 +0300

    working on parse_request_pictures

--- api/api.py
+++ api/api.py
@@ -2320,34 +2320,21 @@ class DataAPI(Resource):
             #if data.lux < 30:
             #    recognize = False
             lowlight = [d.value < 30 for d in data.records if d.ptype == 'light']
-            #if any(lowlight):
-            #    recognize = False
+            if any(lowlight):
+                recognize = False
                 
             db.session.add(data)
             db.session.commit()
+            
             tmpfname = str(uuid.uuid4())
             tmpf = open(tmpfname, 'wb')
             fl = [request.files.get(f) for f in request.files][0]
             tmpf.write(fl.read())
             # Running parse_request_pictures in the background
-            #req_files = [base64.b64encode(request.files.get(f).read()) for f in request.files]
-            #process_thread = threading.Thread(target=parse_request_pictures, args=[data.id, camera_position.id, req_files, flabel, user.login, sensor.uuid, recognize])
-            #app.logger.debug("Start background thread")
-            #process_thread.start()
             
             # Running as celery task
             parse_request_pictures.delay(data.id, camera_position.id, tmpf.name, flabel, user.login, sensor.uuid, recognize)
-            #st.start()
-
-            #res = st.join()
-            #app.logger.debug(res)
-            #parse_request_pictures()
-            # db.session.commit()
-            #if picts:
-            #    for p in picts:
-            #        data.pictures.append(p)
             return "Data added", 200
-        #jsonify(self.schema.dump(data).data)
         abort(404)
 
 
