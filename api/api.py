43c20d248fcebd0ca5511a76250c8a7e99a1d8ca
Andrew <gontchar@gmail.com>
2020-06-05 17:01:06 +0300

    Issue #84 Debugging GET data method

--- api/api.py
+++ api/api.py
@@ -196,7 +196,8 @@ def custom_serializer(data):
     outdata = {"labels": [], "data": {}, "cameras": []}
     for d in data:
         #app.logger.debug("PROCESS DATA {}".format(d.id))
-        outdata['labels'].append(d.ts)
+        if len(d.records) > 0:
+            outdata['labels'].append(d.ts)
         if len(d.cameras) > 0:
             outdata['cameras'].append([{"camlabel":c.camlabel, "id":c.id, "warnings":c.warnings} for c in d.cameras])
         for r in d.records:
