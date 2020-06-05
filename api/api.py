bd68d92d6662ee0a99fa7d3a96bf58861b75589d
Andrew <gontchar@gmail.com>
2020-06-06 01:33:16 +0300

    Issue #84 Debugging GET data method

--- api/api.py
+++ api/api.py
@@ -192,7 +192,7 @@ class SQLAlchemyNoPool(SQLAlchemy):
         super(SQLAlchemy, self).apply_driver_hacks(app, info, options)
 
 def get_count(q):
-    count_q = q.statement.with_only_columns([func.count()]).order_by(None)
+    count_q = q.statement.with_only_columns([sql_func.count()]).order_by(None)
     count = q.session.execute(count_q).scalar()
     return count
         
