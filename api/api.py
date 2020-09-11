2f1f59ea2733c9213a3674c4676b4be3e7161cf8
Andrew <gontchar@gmail.com>
2020-09-12 00:37:47 +0300

    Added ProbeLabels to Data API

--- api/api.py
+++ api/api.py
@@ -232,8 +232,8 @@ def custom_serializer(data, cameras=None):
         # fix missing data
         # find longest probelabel
         # find index of labels that are not in other probelabels
-        max_key, max_value = max(outdata['probelabels'].items(), key = lambda x: len(set(x[1])))
-        app.logger.debug(['Longest key', max_key])
+        #max_key, max_value = max(outdata['probelabels'].items(), key = lambda x: len(set(x[1])))
+        #app.logger.debug(['Longest key', max_key])
     return outdata
 
 
