a988c7cb1b06fbdf581ba4c387f9879a394615b5
Andrew <gontchar@gmail.com>
2020-09-24 10:18:34 +0300

    Testing missing data processing in custom serializer

--- api/api.py
+++ api/api.py
@@ -250,9 +250,14 @@ def custom_serializer(data, cameras=None):
             for dk in outdata['data']:
                 if dk[3:] == k:
                     newdata = [0] * maxlen
+
                     for lab in labels_intersection:
                         label_old_ind = orig_labels.index(lab)
                         label_new_ind = longest_labels.index(lab)
+                        app.logger.debug(["DATA", newdata])
+                        app.logger.debug(["DATA", outdata['data'][dk]])
+                        app.logger.debug(["DATA", outdata['data'][dk][label_old_ind]])
+                        app.logger.debug(["DATA", newdata[label_new_ind]])
                         newdata[label_new_ind] = outdata['data'][dk][label_old_ind]
 
                     # fix mixxing values
