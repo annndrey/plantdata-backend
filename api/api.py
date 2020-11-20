8e9b517c2c3e3702505850568f6af33ac93ef134
Andrew <gontchar@gmail.com>
2020-11-20 12:41:50 +0300

    Issue #103 Added sensorlimits endpoint

--- api/api.py
+++ api/api.py
@@ -1287,7 +1287,7 @@ class SensorTypeAPI(Resource):
         sensors = user.sensors
         reslist = []
         for s in sensors:
-            applogger.debug(s.limits)
+            app.logger.debug(s.limits)
             for d in s.datatypes:
                 reslist.append(d.ptype)
                 
