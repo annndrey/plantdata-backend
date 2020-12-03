f2bcaaa1af83761b3301eada64b89d391ad4e541
Andrew <gontchar@gmail.com>
2020-12-04 00:02:58 +0300

    night & day results

--- api/models.py
+++ api/models.py
@@ -231,7 +231,7 @@ class DataPicture(db.Model):
     def numwarnings(self):
         numwarning = 0
         if self.results:
-            if 9 < self.ts.hour < 22:
+            if 7 < self.ts.hour < 19:
                 numwarning = self.results.count("unhealthy")
         return numwarning
 
