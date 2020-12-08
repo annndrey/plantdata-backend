36783f2a37bcac4cb69eaf3c36d32fc1c444274e
Andrew <gontchar@gmail.com>
2020-12-08 15:42:23 +0300

    probe api auth check

--- api/api.py
+++ api/api.py
@@ -1716,7 +1716,7 @@ class ProbeAPI(Resource):
         suuid = request.args.get('suuid', None)
 
         sensor = db.session.query(Sensor).filter(Sensor.uuid == suuid).first()
-        app.logger.debug(["SENSOR", suuid, sensor, sensor.user, user])
+        app.logger.debug(["SENSOR", suuid, puuid, sensor, user])
         if sensor:
             if sensor.user != user:
                 abort(403)
